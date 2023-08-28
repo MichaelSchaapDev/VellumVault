17b99903a2cdcf0fe048c0625923692c7fa56bcf
Michael Schaap <michael_schaap@outlook.com>
2023-08-28 21:00:46 +0200

    feat(address): raise ValueError for empty address parameters

--- VellumVault/apps/users/domain/value_objects/address.py
+++ VellumVault/apps/users/domain/value_objects/address.py
@@ -1,5 +1,17 @@
 class Address:
     def __init__(self, street: str, city: str, state: str, zip_code: str):
+        if not street:
+            raise ValueError("Street cannot be empty")
+        
+        if not city:
+            raise ValueError("City cannot be empty")
+        
+        if not state:
+            raise ValueError("State cannot be empty")
+        
+        if not zip_code:
+            raise ValueError("Zip code cannot be empty")
+        
         self.street = street
         self.city = city
         self.state = state
