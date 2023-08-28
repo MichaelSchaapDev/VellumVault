a0311a0216b1f69023a6851b2056525bca5e20da
Michael Schaap <michael_schaap@outlook.com>
2023-08-28 22:03:52 +0200

    feat(address): implement equality and zip code length check

--- VellumVault/apps/users/domain/value_objects/address.py
+++ VellumVault/apps/users/domain/value_objects/address.py
@@ -8,11 +8,27 @@ class Address:
         
         if not state:
             raise ValueError("State cannot be empty")
-        
-        if not zip_code:
-            raise ValueError("Zip code cannot be empty")
+
+        # Check if the zip code is numeric
+        if not zip_code.isnumeric():
+            raise ValueError("Zip code must be numeric")
+
+        # Check the length of the zip code
+        if len(zip_code) != 5:
+            raise ValueError("Zip code must be 5 digits long")
         
         self.street = street
         self.city = city
         self.state = state
         self.zip_code = zip_code
+
+    def __eq__(self, other):
+        return (
+            self.street == other.street and
+            self.city == other.city and
+            self.state == other.state and
+            self.zip_code == other.zip_code
+        )
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
