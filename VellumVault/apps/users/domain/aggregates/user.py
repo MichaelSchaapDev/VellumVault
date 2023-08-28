8fd47bba13dcb875dfa23a9180fb02b672471fa3
Michael Schaap <michael_schaap@outlook.com>
2023-08-28 20:56:47 +0200

    feat(user): raise ValueError for empty name

--- VellumVault/apps/users/domain/aggregates/user.py
+++ VellumVault/apps/users/domain/aggregates/user.py
@@ -2,6 +2,9 @@ from ..value_objects.address import Address
 
 class User:
     def __init__(self, user_id: int, name: str, address: Address):
+        if not name:
+            raise ValueError("Name cannot be empty")
+         
         self.user_id = user_id
         self.name = name
         self.address = address
